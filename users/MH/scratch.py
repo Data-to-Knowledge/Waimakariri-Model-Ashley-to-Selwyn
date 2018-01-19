7f6f23e66b186aebc1909c2f6faf60b8fe60263a
MattH <hansonmcoombs@gmail.com>
2018-01-19 15:44:17 +1300

    minor changes

--- users/MH/scratch.py
+++ users/MH/scratch.py
@@ -2,54 +2,50 @@ from __future__ import division
 from users.MH.Waimak_modeling.models.extended_boundry.m_packages import create_wel_package
 from users.MH.Waimak_modeling.models.extended_boundry.m_packages.wel_packages import create_wel_package
 from users.MH.Waimak_modeling.models.extended_boundry.extended_boundry_model_tools import smt
+from users.MH.Waimak_modeling.models.extended_boundry.model_runs.model_run_tools.cwms_index import get_zone_array_index
 from scipy.interpolate import rbf
 import netCDF4 as nc
 import numpy as np
 from core import env
 import shutil
 import os
+import flopy
 import itertools
 from users.MH.Waimak_modeling.models.extended_boundry.model_runs.modpath_sims.extract_data import open_path_file_as_df
 
-path_path = r"C:\Users\MattH\Desktop\test_reverse_modpath_strong\test_reverse.mppth"
-
-drop_names = [
-    'Time_Point_Index',
-    'Cumulative_Time_Step',
-    'Tracking_Time',
-    'Global_X',
-    'Global_Y',
-    'Global_Z',
-    'Grid',
-    'Local_X',
-    'Local_Y',
-    'Local_Z',
-    'Line_Segment_Index',
-]
-print('reading data')
-
-data = open_path_file_as_df(path_path)
-print('simplifying data')
-data.drop(drop_names, 1, inplace=True)
-
-sites = list(itertools.product([1],range(50,150),range(50,160)))
-
 def timeit_function():
-    temp_data = data.set_index(['Layer','Row','Column']).loc[sites].reset_index()
-    return temp_data
+    pass
 def timeit_function2():
-    temp_data = ['{}-{}-{}'.format(l,r,c) for l,r,c in data[['Layer','Row','Column']].itertuples(False,None)]
-    sites2 = ['{}-{}-{}'.format(l,r,c) for l,r,c in sites]
-    idx = np.in1d(temp_data, sites2)
-    out_data = data.loc[idx]
-    return out_data
-
+    pass
 
 
 
 if __name__ == '__main__':
     print 'hello world'
-    temp = timeit_function()
-    temp2 = timeit_function2()
+    bot = smt.calc_elv_db()[1]
+    hds = flopy.utils.HeadFile(r"P:\Groundwater\Waimakariri\Groundwater\Numerical GW model\supporting_data_for_scripts\ex_bd_va_sdp\from_gns\NsmcBase\AW20171024_2_i2_optver\i2\mf_aw_ex.hds").get_alldata()[0][0]
+    idx = get_zone_array_index('waimak')
+    hds[hds>1e20] = np.nan
+    dif1 = hds-bot
+    dif1[dif1<0] = np.nan
+    hds[~idx] = np.nan
+    dif2 = hds-bot
+    dif2[dif2<0] = np.nan
+
+    for nm, data in zip(['all','waimak'],[dif1,dif2]):
+        print nm
+        print 'min: {}'.format(np.nanmin(data))
+        print '1 :{}'.format(np.nanpercentile(data,1))
+        print '5 :{}'.format(np.nanpercentile(data,5))
+        print '25 :{}'.format(np.nanpercentile(data,25))
+        print '50 :{}'.format(np.nanpercentile(data,50))
+        print '75 :{}'.format(np.nanpercentile(data,75))
+        print '95 :{}'.format(np.nanpercentile(data,95))
+        print '99 :{}'.format(np.nanpercentile(data,99))
+        print 'max: {}'.format(np.nanmax(data))
+        print 'u :{}'.format(np.nanmean(data))
+        print 'std: {}'.format(np.nanstd(data))
+
+
 
     print 'done'
