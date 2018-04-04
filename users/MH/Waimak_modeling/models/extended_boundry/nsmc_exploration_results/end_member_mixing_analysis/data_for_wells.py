17e99388008f6f940aad62d4ad3cb101bcdd3f0b
MattH <hansonmcoombs@gmail.com>
2018-04-05 09:42:54 +1200

    setup another EMMA run

--- users/MH/Waimak_modeling/models/extended_boundry/nsmc_exploration_results/end_member_mixing_analysis/data_for_wells.py
+++ users/MH/Waimak_modeling/models/extended_boundry/nsmc_exploration_results/end_member_mixing_analysis/data_for_wells.py
@@ -9,12 +9,15 @@ from core import env
 from pdsql.mssql import rd_sql
 import pandas as pd
 from users.MH.Waimak_modeling.models.extended_boundry.supporting_data_analysis.all_well_layer_col_row import get_all_well_row_col
+import geopandas as gpd
 
 if __name__ == '__main__':
 
     # todo make a list of sites for all wells
     all_wells = get_all_well_row_col()
-    sites = all_wells.index
+    groups = gpd.read_file(r"P:\Groundwater\Waimakariri\Groundwater\Numerical GW model\Model simulations and results\ex_bd_va\n_results\support_shp\cl_o18_data.shp")
+    sites = list(groups.loc[groups.well_group != '','Field1'])
+    mapper = groups.loc[groups.well_group != ''].set_index('Field1').loc[:,'well_group'].to_dict()
     data = rd_sql(server='sql2012dev01',
                   database='hydro',
                   table='HilltopWQMtypes',
@@ -24,12 +27,13 @@ if __name__ == '__main__':
                   to_date='2017-12-31',
                   date_col='CollectionTime',
                   ).reset_index()
+    data.loc[:,'grouper'] = data.loc[:,'SiteID']
+    data = data.replace({'grouper': mapper})
     idx = data.Value.str.contains('<')
     data.loc[:,'Value'] = data.Value.str.replace('<','').astype(float)
     data.loc[idx,'Value'] *= 0.5
-    summary_o18 = data.loc[data.MeasurementType=='Oxygen 18',['SiteID', 'Value']].groupby('SiteID').describe().loc[:,'Value']
-    summary_cl = data.loc[data.MeasurementType=='Chloride',['SiteID', 'Value']].groupby('SiteID').describe().loc[:,'Value']
+    summary_o18 = data.loc[data.MeasurementType=='Oxygen 18',['grouper', 'Value']].groupby('grouper').describe().loc[:,'Value']
+    summary_cl = data.loc[data.MeasurementType=='Chloride',['grouper', 'Value']].groupby('grouper').describe().loc[:,'Value']
     outdata = pd.merge(summary_o18, summary_cl, left_index=True, right_index=True, suffixes=('_o18', '_cl'))
-    outdata = pd.merge(outdata, all_wells, left_index=True, right_index=True)
-    outdata.to_csv(r"C:\Users\MattH\Downloads\cl_o18_data.csv")
+    outdata.to_csv(r"\\gisdata\projects\SCI\Groundwater\Waimakariri\Groundwater\Groundwater Quality\End member mixing model\targets_for_well_adjust\cl_o18_data_grouped.csv")
 
