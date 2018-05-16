8f3fc47f58ce854dfd4e350fbccda82a8ead7fda
MattH <hansonmcoombs@gmail.com>
2018-05-16 15:02:11 +1200

    some more output

--- users/MH/Waimak_modeling/models/extended_boundry/model_runs/n_analysis/amount_landuse_change.py
+++ users/MH/Waimak_modeling/models/extended_boundry/model_runs/n_analysis/amount_landuse_change.py
@@ -15,18 +15,19 @@ from users.MH.Waimak_modeling.models.extended_boundry.extended_boundry_model_too
 from osgeo import gdal
 import os
 
+
 def get_percentages(tif_path):
     levels = [0, 3, 7, 20, 100]
-    lon,lat = smt.get_model_x_y(False)
+    lon, lat = smt.get_model_x_y(False)
     data = gdal.Open(tif_path).ReadAsArray()
     outpath = r"C:\Users\MattH\Downloads\contour_to_shp2.shp"
     contourftoshpfile(data, levels, outpath, lon, lat, extend="both", smooth=False)
     data = gpd.read_file(outpath)
-    data.loc[:,'area'] = data.area
+    data.loc[:, 'area'] = data.area
     outdata = []
-    for _min in [3,7,20]:
+    for _min in [3, 7, 20]:
         try:
-            outdata.append(data.loc[data.loc[:,'min']==_min,'area'].iloc[0]/(1000*1000))
+            outdata.append(data.loc[data.loc[:, 'min'] == _min, 'area'].iloc[0] / (1000 * 1000))
         except:
             outdata.append(0)
     return outdata
@@ -42,17 +43,18 @@ if __name__ == '__main__':
     conservative_shps = ['use_mix']
 
     base_dir = r"P:\Groundwater\Waimakariri\Groundwater\Numerical GW model\Model simulations and results\ex_bd_va\n_results\n_reductions_use_zones_use_scens"
-
+    base_dir = r"P:\Groundwater\Waimakariri\Groundwater\Numerical GW model\Model simulations and results\ex_bd_va\n_results\n_red_mt3d"
     # mixed
-    for ider in ['', '_stream_only','_wdc_wells_only', '_private_wells_only']:
-        outdata = pd.DataFrame(index=pd.MultiIndex.from_product([scenarios,mar_names], names=['option','scenario']),
+    for ider in ['', '_stream_only', '_wdc_wells_only', '_private_wells_only']:
+        outdata = pd.DataFrame(index=pd.MultiIndex.from_product([scenarios, mar_names], names=['option', 'scenario']),
                                columns=['Land areas requiring 3-7% N load reduction',
                                         'Land areas requiring 7-20% N load reduction',
                                         'Land areas requiring >20% N load reduction'])
-        for scen, mar_name in itertools.product(scenarios,mar_names):
-            tif_path = os.path.join(base_dir,'{}_use_mix_{}{}_reduction.tif'.format(scen,mar_name,ider))
+        for scen, mar_name in itertools.product(scenarios, mar_names):
+            tif_path = os.path.join(base_dir, '{}_use_mix_{}{}_reduction.tif'.format(scen, mar_name, ider))
             print(tif_path)
             temp = get_percentages(tif_path)
-            outdata.loc[scen,mar_name]= temp
-        outdata.to_csv(os.path.join(base_dir,'areas_mixed{}.csv'.format(ider)))
-
+            outdata.loc[scen, mar_name] = temp
+        outdata *= 100
+        outdata = outdata.astype(int)
+        outdata.to_csv(os.path.join(base_dir, 'areas_mixed{}.csv'.format(ider)))
---
+++ users/MH/Waimak_modeling/models/extended_boundry/model_runs/n_analysis/plot_reduction_rasters.py
@@ -0,0 +1,77 @@
+# -*- coding: utf-8 -*-
+"""
+Author: matth
+Date Created: 16/05/2018 9:13 AM
+"""
+
+from __future__ import division
+from core import env
+from osgeo.gdal import Open
+import matplotlib.pyplot as plt
+import numpy as np
+from glob import glob
+import os
+from users.MH.Waimak_modeling.models.extended_boundry.extended_boundry_model_tools import smt
+
+
+def plot_reduction_raster(path, outpath=None, title=None):
+    data = Open(path).ReadAsArray()
+
+    fig, (ax) = plt.subplots(figsize=(18.5, 9.5))
+    if title is not None:
+        ax.set_title(title)
+    ax.set_aspect('equal')
+
+    model_xs, model_ys = smt.get_model_x_y()
+    ds = Open(smt.base_map_path)
+    width = ds.RasterXSize
+    height = ds.RasterYSize
+    gt = ds.GetGeoTransform()
+    minx = gt[0]
+    miny = gt[3] + width * gt[4] + height * gt[5]
+    maxx = gt[0] + width * gt[1] + height * gt[2]
+    maxy = gt[3]
+
+    image = ds.ReadAsArray()
+    if image.ndim == 3:  # if a rgb image then plot as greyscale
+        image = image.mean(axis=0)
+    ll = (minx, miny)
+    ur = (maxx, maxy)
+
+    ax.imshow(image, extent=[ll[0], ur[0], ll[1], ur[1]], cmap='gray')  #
+    levels = range(0, 70, 10)
+    cf = ax.contourf(model_xs, model_ys, data, alpha=0.5, levels=range(1, 70, 10), cmap='viridis',extend='max')
+    ticks = np.array(levels)+5
+    ticks[-1] = levels[-1]+1
+    cbar = fig.colorbar(cf, ticks=ticks)
+    labs = ['{}%-{}%'.format(e,e+10) for e in levels]
+    labs[-1] = '>{}%'.format(levels[-1])
+    cbar.ax.set_yticklabels(labs)
+    cbar.ax.tick_params(labelsize=20)
+    xlim,ylim = smt._get_xlim_ylim()
+    ax.set_ylim([5186000, 5214500])
+    ax.set_xlim((1516000,1576500))
+    fig.tight_layout()
+
+    if outpath is not None:
+        fig.savefig(outpath)
+    return fig,ax
+
+
+
+def plot_all_rasters(basedir, outdir):
+    if not os.path.exists(outdir):
+        os.makedirs(outdir)
+    paths = glob(os.path.join(basedir, '*reduction.tif'))
+    for path in paths:
+        fig, ax = plot_reduction_raster(path,
+                                        outpath=os.path.join(outdir, os.path.basename(path).replace('.tif', '.png')),
+                                        title=None)
+        plt.close(fig)
+
+
+
+if __name__ == '__main__':
+    plot_all_rasters(r"P:\Groundwater\Waimakariri\Groundwater\Numerical GW model\Model simulations and results\ex_bd_va\n_results\n_red_mt3d",
+                     r"P:\Groundwater\Waimakariri\Groundwater\Numerical GW model\Model simulations and results\ex_bd_va\n_results\n_red_mt3d\plots")
+    print('done')
