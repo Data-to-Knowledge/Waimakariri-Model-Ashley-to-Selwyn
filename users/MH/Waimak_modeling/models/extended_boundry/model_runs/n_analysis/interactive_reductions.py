c47dfdbfb92889b627b08604046244beef9ff486
MattH <hansonmcoombs@gmail.com>
2018-05-29 14:02:47 +1200

    get interactive stuff working

--- users/MH/Waimak_modeling/models/extended_boundry/model_runs/n_analysis/interactive_reductions.py
+++ users/MH/Waimak_modeling/models/extended_boundry/model_runs/n_analysis/interactive_reductions.py
@@ -20,6 +20,7 @@ from users.MH.Waimak_modeling.models.extended_boundry.extended_boundry_model_too
 from users.MH.Waimak_modeling.models.extended_boundry.model_runs.model_run_tools.cwms_index import get_zone_array_index
 from get_current_pathway_n import get_mt3d_current_pathway_n
 from percentage_reduction_maps import wdc_wells, private_wells
+import pickle
 
 paths_to_shp = {
     'k_harpers': r"P:\Groundwater\Waimakariri\Groundwater\Numerical GW model\Model simulations and results\ex_bd_va\capture_zones_particle_tracking\source_zone_polygons\second_tranche\kaiapoi_harpers_s.shp",
@@ -36,7 +37,12 @@ paths_to_shp = {
 }
 
 
-def get_base_data():
+def get_base_data(recalc=False):
+    pickel_path = os.path.join(smt.pickle_dir, 'supporting_reduction_data.p')
+    if os.path.exists(pickel_path) and not recalc:
+        outdata = pickle.load(open(pickel_path))
+        return outdata
+
     areas = ['k_harpers',
              'k_islands',
              'ohoka',
@@ -50,24 +56,24 @@ def get_base_data():
         pa = 'pc5' if pc5 else 'wopc5'
         current_pathway = get_mt3d_current_pathway_n(add_pc5=pc5)
         for like in likelihoods:
-            outdata['k_harpers_{}_{}'.format(like, pa)] = np.flipud(np.isfinite(
+            outdata['k_harpers_{}_{}'.format(like, pa)] = [np.flipud(np.isfinite(
                 smt.shape_file_to_model_array(paths_to_shp['k_harpers'], 'Id', True))) * current_pathway.loc[
-                                                              'kaiapoi_harpers_s', like]
-            outdata['k_islands_{}_{}'.format(like, pa)] = np.flipud(np.isfinite(
+                                                              'kaiapoi_harpers_s', like]]
+            outdata['k_islands_{}_{}'.format(like, pa)] = [np.flipud(np.isfinite(
                 smt.shape_file_to_model_array(paths_to_shp['k_islands'], 'Id', True))) * current_pathway.loc[
-                                                              'kaiapoi_island_s', like]
-            outdata['ohoka_{}_{}'.format(like, pa)] = np.flipud(np.isfinite(
+                                                              'kaiapoi_island_s', like]]
+            outdata['ohoka_{}_{}'.format(like, pa)] = [np.flipud(np.isfinite(
                 smt.shape_file_to_model_array(paths_to_shp['ohoka'], 'Id', True))) * current_pathway.loc[
-                                                          'ohoka_island_s', like]
-            outdata['court_{}_{}'.format(like, pa)] = np.flipud(np.isfinite(
+                                                          'ohoka_island_s', like]]
+            outdata['court_{}_{}'.format(like, pa)] = [np.flipud(np.isfinite(
                 smt.shape_file_to_model_array(paths_to_shp['court'], 'Id', True))) * current_pathway.loc[
-                                                          'courtenay_kaiapoi_s', like]
-            outdata['cust_{}_{}'.format(like, pa)] = np.flipud(np.isfinite(
+                                                          'courtenay_kaiapoi_s', like]]
+            outdata['cust_{}_{}'.format(like, pa)] = [np.flipud(np.isfinite(
                 smt.shape_file_to_model_array(paths_to_shp['cust'], 'Id', True))) * current_pathway.loc[
-                                                         'cust_skewbridge', like]
-            outdata['cam_{}_{}'.format(like, pa)] = np.flipud(np.isfinite(
+                                                         'cust_skewbridge', like]]
+            outdata['cam_{}_{}'.format(like, pa)] = [np.flipud(np.isfinite(
                 smt.shape_file_to_model_array(paths_to_shp['cam'], 'Id', True))) * current_pathway.loc[
-                                                        'cam_bramleys_s', like]
+                                                        'cam_bramleys_s', like]]
 
             # add private_wells
             private_dir = r"P:\Groundwater\Waimakariri\Groundwater\Numerical GW model\Model simulations and results\ex_bd_va\capture_zones_particle_tracking\source_zone_polygons\private_wells_90_named_right"
@@ -78,7 +84,8 @@ def get_base_data():
                 temp[np.isfinite(temp)] = current_pathway.loc[well, like]
                 private_reductions.append(temp[np.newaxis])
             private_reductions = np.flipud(np.nanmax(np.concatenate(private_reductions, axis=0), axis=0))
-            outdata['private_{}_{}'.format(like, pa)] = private_reductions
+            private_reductions[np.isnan(private_reductions)] = 0
+            outdata['private_{}_{}'.format(like, pa)] = [private_reductions]
 
             # add public_wells
             wdc_dir = r"P:\Groundwater\Waimakariri\Groundwater\Numerical GW model\Model simulations and results\ex_bd_va\capture_zones_particle_tracking\source_zone_polygons\wdc_use_mix"
@@ -89,13 +96,16 @@ def get_base_data():
                 temp[np.isfinite(temp)] = current_pathway.loc[well, like]
                 wdc_reductions.append(temp[np.newaxis])
             wdc_reductions = np.flipud(np.nanmax(np.concatenate(wdc_reductions, axis=0), axis=0))
-            outdata['public_{}_{}'.format(like, pa)] = wdc_reductions
+            wdc_reductions[np.isnan(wdc_reductions)] = 0
+            outdata['public_{}_{}'.format(like, pa)] = [wdc_reductions]
 
-    outdata['waimak'] = np.flipud(np.isfinite(smt.shape_file_to_model_array(paths_to_shp['waimak'], 'Id', True)).astype(float))
+    outdata['waimak'] = [np.flipud(
+        np.isfinite(smt.shape_file_to_model_array(paths_to_shp['waimak'], 'Id', True)).astype(float))]
+    pickle.dump(outdata, open(pickel_path, 'w'))
     return outdata
 
 
-def create_reduction_reason(tar_ds, pred_ds, version='reduction'):
+def create_reduction_reason(ds, version='reduction'):
     areas = ['k_harpers',
              'k_islands',
              'ohoka',
@@ -107,18 +117,20 @@ def create_reduction_reason(tar_ds, pred_ds, version='reduction'):
 
     outdata = []
     for area in areas:
-        likelihood = tar_ds.data['lik_{}'.format(area)][0]
-        pa = 'pc5' if tar_ds.data['pc5pa'.format(area)][0] else 'wopc5'
-        target = tar_ds.data['tar_{}'.format(area)][0]
-        org = pred_ds.data['{}_{}_{}'.format(area, likelihood, pa)]
+        likelihood = ds['lik_{}'.format(area)][0][0]
+        pa = 'pc5' if ds['pc5pa'.format(area)][0][0] == 1 else 'wopc5'
+        target = ds['tar_{}'.format(area)][0][0]
+        org = ds['{}_{}_{}'.format(area, likelihood, pa)][0]
         org = (1 - target / org) * 100
         org[org < 0] = 0
         outdata.append(org[np.newaxis])
-    org = pred_ds.data['waimak'].astype(float)
+    org = ds['waimak'][0].astype(float)
     org[np.isclose(org, 0)] = 0
-    outdata.append((org * tar_ds.data['tar_waimak'][0])[np.newaxis])
+    outdata.append((org * ds['tar_waimak'][0][0])[np.newaxis])
     if version == 'reduction':
-        return np.nanmax(np.concatenate(outdata, axis=0), axis=0)
+        out = np.nanmax(np.concatenate(outdata, axis=0), axis=0)
+        out[np.isclose(out, 0)] = np.nan
+        return out
     else:
         raise NotImplementedError('version: {}'.format(version))
 
@@ -151,32 +163,33 @@ if __name__ == '__main__':
 
     targets = {
         # targets
-        'tar_k_harpers': [6.9],
-        'tar_k_islands': [6.9],
-        'tar_ohoka': [6.9],
-        'tar_court': [6.9],
-        'tar_cust': [6.9],
-        'tar_cam': [2.4],
-        'tar_public': [999],
-        'tar_private': [999],
-        'tar_waimak': [0],
+        'tar_k_harpers': [np.full((364, 365),6.9).flatten()],
+        'tar_k_islands': [np.full((364, 365),6.9).flatten()],
+        'tar_ohoka': [np.full((364, 365),6.9).flatten()],
+        'tar_court': [np.full((364, 365),6.9).flatten()],
+        'tar_cust': [np.full((364, 365),6.9).flatten()],
+        'tar_cam': [np.full((364, 365),2.4).flatten()],
+        'tar_public': [np.full((364, 365),999).flatten()],
+        'tar_private': [np.full((364, 365),999).flatten()],
+        'tar_waimak': [np.full((364, 365),0).flatten()],
         # pc5pa
-        'pc5pa': [True],
+        'pc5pa': [np.full((364, 365),1).flatten()],
 
         # liklyhoods
-        'lik_k_harpers': ['50%'],
-        'lik_k_islands': ['50%'],
-        'lik_ohoka': ['50%'],
-        'lik_court': ['50%'],
-        'lik_cust': ['50%'],
-        'lik_cam': ['50%'],
-        'lik_public': ['50%'],
-        'lik_private': ['50%']
+        'lik_k_harpers': [np.full((364, 365), '50%').flatten()],
+        'lik_k_islands': [np.full((364, 365), '50%').flatten()],
+        'lik_ohoka': [np.full((364, 365), '50%').flatten()],
+        'lik_court': [np.full((364, 365), '50%').flatten()],
+        'lik_cust': [np.full((364, 365), '50%').flatten()],
+        'lik_cam': [np.full((364, 365), '50%').flatten()],
+        'lik_public': [np.full((364, 365), '50%').flatten()],
+        'lik_private': [np.full((364, 365), '50%').flatten()]
 
     }
-    target_source = ColumnDataSource(targets)
-    prediction_source = ColumnDataSource(
-        get_base_data())  # todo think about this cannot handle any deeper objects/abstraction
+
+    targets.update(get_base_data())
+    targets['image'] = [create_reduction_reason(targets, version='reduction')]
+    data_source = ColumnDataSource(targets)
 
     # I will need to have a use prediction souce which has keys (ohoka ect) and values (2d numpy array)
     # I will also need to have a base source for each group (waimak, ect) which has keys: 25%: array or value.
@@ -224,79 +237,129 @@ if __name__ == '__main__':
 
     # plot reduction
     y = smt.uly - smt.grid_space * smt.rows
-    test = create_reduction_reason(target_source, prediction_source, version='reduction')
-    img = p.image(image=[test], x=smt.ulx, y=y, dw=smt.cols * smt.grid_space, dh=smt.rows * smt.grid_space,
-                  palette="Plasma256",
-                  alpha=0.5)  # todo try to figure out different plotting levels, and how to set a datasource for reason
-    image_ds = img.data_source
+    img = p.image(image='image', x=smt.ulx, y=y, dw=smt.cols * smt.grid_space, dh=smt.rows * smt.grid_space,
+                  palette="Plasma256", source=data_source,
+                  alpha=0.5)  # todo try to figure out different plotting levels
 
 
-    def callback_tar(source=image_ds, window=None):  # todo I may need to pass the
+    def callback_tar(source=data_source, window=None):
         # update the target data source with the new target
-        #new_tar = cb_obj.value
-        #name = cb_obj.name
-        #if 'lik' in name:  # pass the likelihoods straight across
-        #    val = new_tar
-        #elif name == 'tar_waimak':
-        #    if new_tar == 'current':
-        #        val = 0
-        #    else:
-        #        val = 28
-        #elif name == 'tar_public' or name == 'tar_private':
-        #    if new_tar == 'no target':
-        #        val = 999
-        #    else:
-        #        val = float(new_tar)
-        #elif name == 'pc5pa':
-        #    if new_tar == 'With PC5PA':
-        #        val = True
-        #    else:
-        #        val = False
-        #else:
-        #    val = float(new_tar)
-        #tar_source.data[name] = val
-#
-        #areas = ['k_harpers',
-        #         'k_islands',
-        #         'ohoka',
-        #         'court',
-        #         'cust',
-        #         'cam',
-        #         'public',
-        #         'private']
-#
-        #outdata = []
-        #for area in areas:
-        #    likelihood = tar_source.data['lik_{}'.format(area)][0]
-        #    pa = 'pc5' if tar_source.data['pc5pa'.format(area)][0] else 'wopc5'
-        #    target = tar_source.data['tar_{}'.format(area)][0]
-        #    org = pred_source.data['{}_{}_{}'.format(area, likelihood, pa)]
-        #    for i in range(364):
-        #        for j in range(365):
-        #            org[i,j] = (1 - target / org[i,j]) * 100
-        #            if org[i,j] < 0:
-        #                org[i,j] = 0
-        #    outdata.append(org)
-        #org = pred_source.data['waimak'] #todo make sure this is not a list
-        #for i in range(364):
-        #    for j in range(365):
-        #        org[i, j] = org[i,j] * tar_source.data['tar_waimak'][0]
-        #outdata.append(org)
-        # temp = im_source.data['image'][0]
-        temp2 = []
-        for i in range(364):
-            temp3 = []
-            for j in range(365):
-                temp3.append(i)
-            temp2.append(temp3)
-        source.data.image = temp2 # this breaks it for some reason, but you see a change
-                #source.data['image'][i][j] = 0 #try new not mutating
-                #max(outdata[0][i][j],outdata[1][i][j],outdata[2][i][j],outdata[3][i][j],outdata[4][i][j],
-                #outdata[5][i][j],outdata[6][i][j]) #todo check how many arrays
+        new_tar = cb_obj.value
+        name = cb_obj.name
+        print(name)
+        print(new_tar)
+        if 'lik' in name:  # pass the likelihoods straight across
+            val = new_tar
+        elif name == 'tar_waimak':
+            if new_tar == 'current':
+                val = 0
+            else:
+                val = 28
+        elif name == 'tar_public' or name == 'tar_private':
+            if new_tar == 'no target':
+                val = 999
+            else:
+                val = float(new_tar)
+        elif name == 'pc5pa':
+            if new_tar == 'With PC5PA':
+                val = 1
+            else:
+                val = 0
+        else:
+            val = float(new_tar)
+
+        new_val = []
+        for i in range(364 * 365):
+            new_val.append(val)
+        print('old')
+        print(source.data[name])
+
+        source.data[name] = [new_val]
+        print('new')
+        print(source.data[name])
+
         source.change.emit()
-    #todo put everything in a singel data source
-    # todo add the update on change thing
+        print('passes the change.emit')
+        areas = ['k_harpers',
+                 'k_islands',
+                 'ohoka',
+                 'court',
+                 'cust',
+                 'cam',
+                 'public',
+                 'private']
+
+        outdata = []
+        for area in areas:
+            likelihood = source.data['lik_{}'.format(area)][0][0]
+            print('likelihood: {}'.format(area))
+            print(likelihood)
+            print(source.data['lik_{}']) # harpers and islands seem to have different structure...
+            pa = 'pc5' if source.data['pc5pa'.format(area)][0][0] == 1 else 'wopc5'
+            target = source.data['tar_{}'.format(area)][0][0]
+            temp = '{}_{}_{}'.format(area, likelihood, pa)
+            print(temp)
+            org = source.data['{}_{}_{}'.format(area, likelihood, pa)]
+            print(org)
+            org = org[0]
+            new_map = []
+            for i in range(364 * 365):
+                temp = (1 - target / org[i]) * 100
+                if temp < 0:
+                    temp = 0
+                new_map.append(temp)
+            outdata.append(new_map)
+        org = source.data['waimak'][0]
+        new_map = []
+        for i in range(364 * 365):
+            temp = org[i]
+            if temp < 0:
+                temp = 0
+            new_map.append(temp)
+        outdata.append(new_map)
+        print('outdata')
+        print(outdata)
+        new_image = []
+        for i in range(364 * 365):
+            temp = max(outdata[0][i], outdata[1][i], outdata[2][i], outdata[3][i], outdata[4][i],
+                       outdata[5][i], outdata[6][i], outdata[7][i], outdata[8][i])
+            if temp == 0:
+                temp = float('nan')
+            new_image.append(temp)
+
+        print('old image')
+        print(source.data.image)
+
+        source.data['image'] = [new_image]  # the format for souce.data.org is: [smt.get_empty_model_grid().flatten()]
+
+        print('new image')
+        print(source.data.image)
+        source.change.emit()
+
+
+    # apply the js on changes
+    sel_pa.js_on_change('value', CustomJS.from_py_func(callback_tar))
+
+    # targets
+    sel_tar_kai_harp.js_on_change('value', CustomJS.from_py_func(callback_tar))
+    sel_tar_kai_isl.js_on_change('value', CustomJS.from_py_func(callback_tar))
+    sel_tar_ohoka.js_on_change('value', CustomJS.from_py_func(callback_tar))
+    sel_tar_cour.js_on_change('value', CustomJS.from_py_func(callback_tar))
+    sel_tar_cust.js_on_change('value', CustomJS.from_py_func(callback_tar))
+    sel_tar_cam.js_on_change('value', CustomJS.from_py_func(callback_tar))
+    sel_tar_public.js_on_change('value', CustomJS.from_py_func(callback_tar))
     sel_tar_private.js_on_change('value', CustomJS.from_py_func(callback_tar))
+    sel_tar_wai.js_on_change('value', CustomJS.from_py_func(callback_tar))
+
+    # likelyhoods
+    sel_likely_kai_harp.js_on_change('value', CustomJS.from_py_func(callback_tar))
+    sel_likely_kai_isl.js_on_change('value', CustomJS.from_py_func(callback_tar))
+    sel_likely_ohoka.js_on_change('value', CustomJS.from_py_func(callback_tar))
+    sel_likely_cour.js_on_change('value', CustomJS.from_py_func(callback_tar))
+    sel_likely_cust.js_on_change('value', CustomJS.from_py_func(callback_tar))
+    sel_likely_cam.js_on_change('value', CustomJS.from_py_func(callback_tar))
+    sel_likely_public.js_on_change('value', CustomJS.from_py_func(callback_tar))
+    sel_likely_private.js_on_change('value', CustomJS.from_py_func(callback_tar))
 
     layout2 = layout([p,
                       sel_pa,
