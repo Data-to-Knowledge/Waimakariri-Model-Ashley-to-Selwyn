cd350206c7f24a79a262d2bd6cc46eae0e4c5e44
MattH <hansonmcoombs@gmail.com>
2018-06-13 15:32:45 +1200

    plot short term reduction strategy

--- users/MH/Waimak_modeling/models/extended_boundry/supporting_data_analysis/data_pull_for_lagoon.py
+++ users/MH/Waimak_modeling/models/extended_boundry/supporting_data_analysis/data_pull_for_lagoon.py
@@ -12,11 +12,13 @@ import pandas as pd
 
 if __name__ == '__main__':
     m = get_model('NsmcBase')
-    row,col = smt.convert_coords_to_matix(1575962,5203005)
+    #row,col = smt.convert_coords_to_matix(1575962,5203005) # for lagoon
+    row,col = smt.convert_coords_to_matix(1556717,5193196) # for infiltration test #todo
     elv_db = smt.calc_elv_db()
 
     outdata = pd.DataFrame(index=range(11))
     outdata.loc[:,'kh'] = m.upw.hk.array[:,row,col]
     outdata.loc[:,'kv'] = m.upw.vka.array[:,row,col]
     outdata.loc[:,'thickness'] = (elv_db[0:-1] - elv_db[1:])[:,row,col]
-    outdata.to_csv(r"C:\Users\MattH\Downloads\data_for_lagoon.csv")
+    #outdata.to_csv(r"C:\Users\MattH\Downloads\data_for_lagoon.csv") # for lagoon
+    outdata.to_csv(r"T:\Temp\temp_gw_files\data_for_itest.csv") # for infiltration test
